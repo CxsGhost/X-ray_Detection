6b60ede36f4cebc8cdf47fa496db462523156f8d
CxsGhost <1659101919@qq.com>
2022-03-10 18:30:22 +0800

    11

--- 云计算与大数据课/案例2/echarts.py
+++ 云计算与大数据课/案例2/echarts.py
@@ -1,10 +1,8 @@
 import random
-
 import pandas as pd
 import pyecharts.options as opts
 from pyecharts.charts import Line
 import datetime
-#%%
 
 def get_time(x):
     return str(datetime.datetime.strptime(x, '%Y-%m-%d'))[:10]
@@ -17,13 +15,12 @@ date_list = sorted(list(set(date_list)))
 veget = data['variety'].unique()
 random.shuffle(veget)
 data = data.groupby('variety')
+
 #%%
 
-line = (
-    Line()
+line = (Line()
         .add_xaxis(xaxis_data=date_list)
-        .set_global_opts(title_opts=opts.TitleOpts(title='蔬菜价格走势图-1'))
-)
+        .set_global_opts(title_opts=opts.TitleOpts(title='蔬菜价格走势图-1')))
 for v in range(20):
     v = veget[v]
     temp = data.get_group(v)[['date', 'price']]
@@ -31,21 +28,17 @@ for v in range(20):
     temp = temp.values
     x = temp[:, 0]
     y = temp[:, 1]
-    line_1 = (
-            Line()
-            .add_xaxis(xaxis_data=x)
-            .add_yaxis(series_name=v, y_axis=y, is_symbol_show=True,
-                       label_opts=opts.LabelOpts(is_show=False),
-                       is_connect_nones=True)
-            )
+    line_1 = (Line()
+              .add_xaxis(xaxis_data=x)
+              .add_yaxis(series_name=v, y_axis=y, is_symbol_show=True,
+                         label_opts=opts.LabelOpts(is_show=False),
+                         is_connect_nones=True))
     line.overlap(line_1)
 line.render('./render1.html')
 
-line = (
-    Line()
+line = (Line()
         .add_xaxis(xaxis_data=date_list)
-        .set_global_opts(title_opts=opts.TitleOpts(title='蔬菜价格走势图-2'))
-)
+        .set_global_opts(title_opts=opts.TitleOpts(title='蔬菜价格走势图-2')))
 for v in range(20, 40):
     v = veget[v]
     temp = data.get_group(v)[['date', 'price']]
@@ -53,12 +46,10 @@ for v in range(20, 40):
     temp = temp.values
     x = temp[:, 0]
     y = temp[:, 1]
-    line_1 = (
-            Line()
-            .add_xaxis(xaxis_data=x)
-            .add_yaxis(series_name=v, y_axis=y, is_symbol_show=True,
-                       label_opts=opts.LabelOpts(is_show=False),
-                       is_connect_nones=True)
-            )
+    line_1 = (Line()
+              .add_xaxis(xaxis_data=x)
+              .add_yaxis(series_name=v, y_axis=y, is_symbol_show=True,
+                         label_opts=opts.LabelOpts(is_show=False),
+                         is_connect_nones=True))
     line.overlap(line_1)
 line.render('./render2.html')
